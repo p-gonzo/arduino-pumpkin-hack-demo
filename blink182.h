7b63dc84404250b19f1dc3631849f5020cc4fcee
Sean G <goofygrin@gmail.com>
2022-10-26 20:15:57 -0600

    Create blink182.h
    For the avs fans, just include it in

---
+++ blink182.h
@@ -0,0 +1,48 @@
+int melody[] = {
+NOTE_E5,4,//all
+NOTE_C5,2,//the
+REST, 4,
+NOTE_B4,4,//small
+NOTE_C5,2 //things
+,REST, 4
+,NOTE_E5, 4 //true
+,NOTE_C5,2 //care
+,REST,4
+,NOTE_B4,4//truth
+,NOTE_C5,2//brings
+,REST, 4
+/*
+,NOTE_E5, 4 //I'll
+,NOTE_C5,2 //take
+,REST,4
+,NOTE_B4,4//one
+,NOTE_C5,2//lift
+,REST, 4
+,NOTE_E5, 4 //your
+,NOTE_C5,2 //ride
+,REST,4
+,NOTE_B4,4//best
+,NOTE_C5,2//trip
+*/
+,NOTE_E5,8 //say
+,NOTE_E5,8 //it
+,NOTE_E5,4 //ain't
+,NOTE_E5,4 //so
+,REST,4
+,NOTE_E5,8//I
+,NOTE_F5,8//will
+,NOTE_E5,4//not
+,NOTE_D5,4//go
+,REST,4
+,NOTE_D5,8//turn
+,NOTE_D5,8//the
+,NOTE_D5,4//lights
+,NOTE_C5,4//off
+,REST,4
+,NOTE_F5,8//car
+,NOTE_F5,8//ry
+,NOTE_F5,4//me
+,NOTE_C5,4//home
+,REST,4
+
+};
